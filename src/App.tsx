fb67966d270394427bb1fb922a84a857e3f7742e
Daniel Clarke <dawn0966@gmail.com>
2022-01-13 12:27:37 +1000

    Update to Yarn2 format

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,32 @@
-import Runsheet from "./components/client/Runsheet";
-import { createTheme, ThemeProvider } from "@material-ui/core";
-//import styled from "@emotion/styled";
-import SyncState from "./components/client/SyncState";
-import init from "./components/common/Init";
-
-import { experimentalStyled as styled } from "@material-ui/core/styles";
-import Box from "@material-ui/core/Box";
-import ViewRunsheet from "./views/Runsheet";
+import {
+    createTheme,
+    ThemeProvider,
+    experimentalStyled as styled,
+    Box
+} from "@mui/material";
 
 const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
+    palette: {
+        mode: "dark"
+    }
 });
 
 const Background = styled(Box)`
-  width: 100%;
-  height: 100vh;
-  background-color: ${({ theme }) => theme.palette.background.paper};
-  position: fixed;
-  overflow-x: hidden;
+    width: 100%;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.palette.background.paper};
+    position: fixed;
+    overflow-x: hidden;
 `;
 
 function App() {
-  init();
-  return (
-    <ThemeProvider theme={theme}>
-      <Background>
-        <SyncState>
-          <ViewRunsheet />
-        </SyncState>
-      </Background>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Background>
+              
+            </Background>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/components/client/Timer.tsx
+++
@@ -1,9 +0,0 @@
-import {stringify,INVALID as INVALID_POINT} from "../common/TimePoint";
-import {Fragment} from "react"
-
-
-const Timer = (props:any) => {
-    return (<Fragment>{stringify(INVALID_POINT)}</Fragment>)
-}
-
-export default Timer;

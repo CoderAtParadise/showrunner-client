a92c7cb7c385a30400d40c79cc82e213c4f9f5d3
Daniel Clarke <dawn0966@gmail.com>
2022-05-19 16:41:59 +1000

    update dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,8 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{RunEvent};
-
 fn main() {
   tauri::Builder::default()
-    .build(tauri::generate_context!())
+    .run(tauri::generate_context!())
     .expect("error while running tauri application")
-    .run(|_app_handle, e| {
-      if let RunEvent::MainEventsCleared = e {
-        //  println!("Derp");
-      }
-    });
 }
